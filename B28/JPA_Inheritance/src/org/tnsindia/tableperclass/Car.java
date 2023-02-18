9cfe2f2dd8759a91c3b55583363dfb22a7fe742e
kruthika <113121037+kruthika444@users.noreply.github.com>
2023-02-18 19:09:24 +0530

    Add files via upload

---
+++ B28/JPA_Inheritance/src/org/tnsindia/tableperclass/Car.java
@@ -0,0 +1,25 @@
+package JPA_Inheritance.src.org.tnsindia.tableperclass;
+package org.tnsindia.tableperclass;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//child class
+
+@Entity
+@Table(name = "Car")
+public class Car extends Vehicle {
+
+    // data members
+    private double price;
+
+    // setters and getters method
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
